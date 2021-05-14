fe6a372a3545f8a953689fb3048e5bff744b7258
ele1992 <tjin.yip@gmail.com>
2021-05-14 14:30:35 +0200

    setting up router and homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./HomePage/HomePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
